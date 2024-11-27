71e4a6ea121afe22fadd920bb041dfddaa68ebcd
Eleni Oikonomou <oikonomou.g.eleni@outlook.com>
2024-11-27 15:45:46 -0800

    css_styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,8 @@ export default function Footer() {
   return (
     <div>
     <h2>Martian Landscape</h2>
+    <h1>APOD PROJECT</h1>
+    <button><i className="fa-solid fa-circle-info"></i></button>
     </div>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <div>
+        <h2>The Brutal Martian Landscape</h2>
+            <div>
+                <p>Description</p>
+                <p>This image was captured from NASA</p>
+            </div>
+        
+    </div>
   )
 }
