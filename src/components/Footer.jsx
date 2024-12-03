42b132f938fc3c6e3623fd358d7d8c65691527e4
Eleni Oikonomou <oikonomou.g.eleni@outlook.com>
2024-12-02 18:06:46 -0800

    sidebar_css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>
-    <h2>Martian Landscape</h2>
-    <h1>APOD PROJECT</h1>
-    <button><i className="fa-solid fa-circle-info"></i></button>
-    </div>
+    <footer>
+        <div className="bgGradient"></div>
+        <div>    
+            <h2>Martian Landscape</h2>
+            <h1>APOD PROJECT</h1>
+        </div>           
+        <button>
+        <i className="fa-solid fa-circle-info"></i>
+        </button>        
+    </footer>
+    
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 
 export default function Sidebar() {
   return (
-    <div>
+    <div className='sidebar'>
+    <div className='bgOverlay'></div>
+    <div className='sidebarContents'>
         <h2>The Brutal Martian Landscape</h2>
             <div>
                 <p>Description</p>
                 <p>This image was captured from NASA</p>
-            </div>
-        
+            </div>    
+            < button></button>
+            </div>    
     </div>
   )
 }
