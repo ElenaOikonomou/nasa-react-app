a625d719a1eac1422600610293a8c56c96b89131
Eleni Oikonomou <oikonomou.g.eleni@outlook.com>
2024-12-05 15:13:44 -0800

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import Main from "./components/Main"
 import Sidebar from "./components/Sidebar"
 import Footer from "./components/Footer"
+import { useState } from "react"
 function App() {
+  const [showModal, setShowModal] = useState(true)
+
+  function handleToggleModal(){
+    setShowModal(!showModal)
+  }
   
 
   return (
     <>
       <Main />
-      <Sidebar />     
-      <Footer />
+      {showModal && (<Sidebar />)}     
+      <Footer showModal={showModal} handleToggleModal={handleToggleModal}/>
     </>
   )
 }
